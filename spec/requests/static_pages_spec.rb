40659fe2890aae0816fa5a11835c9f28209f8b06
Mike Westwood <mikewestwoo1979@gmail.com>
2013-05-02 10:15:32 +0100

    Completed changes to RSPEC tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -12,7 +14,7 @@ describe "Static pages" do
     it "should have the title 'Ruby on Rails Tutorial Sample App | Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "#{base_title} | Home")
     end
   end
 
@@ -26,7 +28,7 @@ describe "Static pages" do
     it "should have the title 'Ruby on Rails Tutorial Sample App | Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "#{base_title} | Help")
     end
   end
 
@@ -40,7 +42,7 @@ describe "Static pages" do
     it "should have the title 'Ruby on Rails Tutorial Sample App | About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "#{base_title} | About Us")
     end
   end
 
@@ -49,13 +51,13 @@ describe "Static pages" do
 
     it "should have the h1 'Contact Us'" do
       visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact Us')
+      page.should have_selector('h1', :text => 'Contact')
     end
 
-    it "should have the title 'Ruby on Rails Tutorial Sample App | Contact Us'" do
+    it "should have the title 'Ruby on Rails Tutorial Sample App | Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+                    :text => "#{base_title} | Contact")
     end
   end
 
